e1c255a838b674e1f1aa74360f7a75b4b1c0445b
JuniorInaletsile <juniorlimbani@gmail.com>
2026-09-10 10:48:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+const menuIcon = document.querySelector(".menu-icon");
+const navbar = document.querySelector(".navbar");
 
+menuIcon.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+
+    const icon = menuIcon.querySelector("i");
+
+    if (navbar.classList.contains("active")) {
+        icon.classList.replace("bx-menu", "bx-x");
+    } else {
+        icon.classList.replace("bx-x", "bx-menu");
+    }
+});
+
+
+/* Close menu when a link is clicked */
+
+document.querySelectorAll(".navbar a").forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        navbar.classList.remove("active");
+
+        const icon = menuIcon.querySelector("i");
+
+        icon.classList.replace("bx-x", "bx-menu");
+
+    });
+
+});
